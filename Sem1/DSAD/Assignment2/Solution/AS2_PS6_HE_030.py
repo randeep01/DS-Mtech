1941445c10ecea8195053c00e621db963627ea70
IIT11 <sid.usit@gmail.com>
2020-02-10 22:09:05 +0530

    Update AS2_PS6_HE_030.py

--- Sem1/DSAD/Assignment2/Solution/AS2_PS6_HE_030.py
+++ Sem1/DSAD/Assignment2/Solution/AS2_PS6_HE_030.py
@@ -194,31 +194,45 @@ class Graph():
   
         printArr(dist,V) 
   
-  
+
+def getNumNodes(fileText):
+    nodeTraversalList = []
+    for line in fileText:
+        if "/" in line:
+#           Process line as a node
+            line.replace(" ","")
+            nodeDistSplit = line.split("/")
+            print(nodeDistSplit)
+            nodeTraversalList.append(nodeDistSplit[0],nodeDistSplit[1])
+        else:
+            print(line)
+    return len(set(nodeTraversalList))
+
 # Driver program to test the above functions 
 def main():
     parentFolderPath = ''
     inputFileName = 'inputPS6.txt'
 #    outputFile = 'outputPS6.txt'
     with open(parentFolderPath+inputFileName, 'r') as inpf:
-        for line in inpf.readlines():
-            print(line)
-#    graph = Graph(9) 
-#    graph.addEdge(0, 1, 4) 
-#    graph.addEdge(0, 7, 8) 
-#    graph.addEdge(1, 2, 8) 
-#    graph.addEdge(1, 7, 11) 
-#    graph.addEdge(2, 3, 7) 
-#    graph.addEdge(2, 8, 2) 
-#    graph.addEdge(2, 5, 4) 
-#    graph.addEdge(3, 4, 9) 
-#    graph.addEdge(3, 5, 14) 
-#    graph.addEdge(4, 5, 10) 
-#    graph.addEdge(5, 6, 2) 
-#    graph.addEdge(6, 7, 1) 
-#    graph.addEdge(6, 8, 6) 
-#    graph.addEdge(7, 8, 7) 
-#    graph.dijkstra(0)
+        fileText = inpf.readlines()
+    print(getNumNodes(fileText))
+                
+    graph = Graph(9) 
+    graph.addEdge(0, 1, 4) 
+    graph.addEdge(0, 7, 8) 
+    graph.addEdge(1, 2, 8) 
+    graph.addEdge(1, 7, 11) 
+    graph.addEdge(2, 3, 7) 
+    graph.addEdge(2, 8, 2) 
+    graph.addEdge(2, 5, 4) 
+    graph.addEdge(3, 4, 9) 
+    graph.addEdge(3, 5, 14) 
+    graph.addEdge(4, 5, 10) 
+    graph.addEdge(5, 6, 2) 
+    graph.addEdge(6, 7, 1) 
+    graph.addEdge(6, 8, 6) 
+    graph.addEdge(7, 8, 7) 
+    graph.dijkstra(0)
 
 if __name__=="__main__":
     main()
